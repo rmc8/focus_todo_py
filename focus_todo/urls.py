4a8b2d56db6d022918c7be7df8ed4ddfb5f441c2
rmc8 <kmprmc@gmail.com>
2023-02-18 17:33:03 +0900

    [ADD] logout

--- focus_todo/urls.py
+++ focus_todo/urls.py
@@ -4,4 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class FocusToDoUrlPath:
     login: str = "v63/user/login"
-    logout: str = "v60/property?client=chrome"
+    logout: str = "v60/property?client="
